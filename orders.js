6e9c4d5d1dd12894203ece00e37933474a147f0f
Sarthak Srivastava <sarthakmahi345@gmail.com>
2025-12-25 15:15:32 +0530

    Update database import path in orders.js

--- orders.js
+++ orders.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Database = require('../models/database');
+const Database = require('./database');
 const { v4: uuidv4 } = require('uuid');
 const router = express.Router();
 
