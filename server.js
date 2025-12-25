7f582a79b4aad433a9e97c875aa9496b17a5e6c4
Sarthak Srivastava <sarthakmahi345@gmail.com>
2025-12-25 14:18:53 +0530

    Fix database import path in server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const Database = require('./database");
+const Database = require('./database.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
