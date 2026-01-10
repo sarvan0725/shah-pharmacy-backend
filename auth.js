b61c688e7fb4456334d976d28fb123396f82176b
Sarthak Srivastava <sarthakmahi345@gmail.com>
2026-01-10 13:13:53 +0530

    Fix import path for Database module in auth.js

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Database = require('../models/database');
+const Database = require('./database');
 const nodemailer = require('nodemailer');
 const jwt = require('jsonwebtoken');
 const router = express.Router();
