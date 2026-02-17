1ae2cc93e19182ac3e1818fabf809abc7dc83909
Sarthak Srivastava <sarthakmahi345@gmail.com>
2026-02-17 23:57:46 +0530

    Update database.js

--- database.js
+++ database.js
@@ -1,3 +1,13 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("✅ MongoDB Connected"))
+.catch(err => console.error("❌ MongoDB Error:", err));
+
+
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
