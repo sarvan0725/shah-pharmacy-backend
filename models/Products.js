e128ec811c600433e963fa6140d17b82aee60473
Sarthak Srivastava <sarthakmahi345@gmail.com>
2026-02-19 01:31:51 +0530

    Create Products.js

---
+++ models/Products.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  category_id: { type: Number, required: true },
+  price: { type: Number, required: true },
+  discount_price: { type: Number, default: 0 },
+  stock: { type: Number, required: true },
+  image: { type: String, default: null },
+  description: { type: String, default: '' },
+  brand: { type: String, default: '' },
+  unit: { type: String, default: 'piece' },
+  is_active: { type: Boolean, default: true }
+}, { timestamps: true });
+
+module.exports =
+  mongoose.models.Product || mongoose.model('Product', productSchema);
