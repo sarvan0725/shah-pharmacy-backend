e0804b00fe3d12776b50966ac028eac87fe71f5d
Sarthak Srivastava <sarthakmahi345@gmail.com>
2026-02-18 13:30:49 +0530

    Create order.js

---
+++ models/order.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema({
+  customerName: String,
+  phone: String,
+  items: [
+    {
+      productId: String,
+      name: String,
+      price: Number,
+      qty: Number
+    }
+  ],
+  totalAmount: Number,
+  status: {
+    type: String,
+    default: "pending"
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
